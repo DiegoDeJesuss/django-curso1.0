be6c58c2501ddaec03ba16b0520171699e2a3f00
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-03-28 20:23:02 -0300

    alteraçoes na views e nos forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 def cadastro(request):
     return HttpResponse('Ola mundo')
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        queixa = request.POST.get('queixa')
+        
+        
+        if len(nome) <= 2:
+            return redirect('clientes')
+        
+        if queixa not in {'D', 'A'}:
+            return redirect('clientes')
+        
+        return HttpResponse(f'{nome}, {email}, {queixa}')
 
