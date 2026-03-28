005f696f05d7fc197c5f0767992296420b8ac5a2
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-03-28 19:32:56 -0300

    começando a criar o banco de dados.

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+   
+class Cliente(models.Model):
+    queixa_choices = (
+        ('D', 'Depressao'),
+        ('A', 'Ansiedade'),
+    )
 
-# Create your models here.
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    queixa = models.CharField(max_length=1, choices=queixa_choices)
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+        
+        
+    def __str__(self):
+        return self.nome        
+     
